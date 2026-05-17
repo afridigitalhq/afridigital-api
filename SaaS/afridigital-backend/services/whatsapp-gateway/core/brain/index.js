d722c2ab01fd8f6be110df7f458253f659522b1f
AfriLove Team <afriloveteam@gmail.com>
2026-05-17 15:51:11 +0100

    fix: correct absolute brain v5 module resolution path

--- SaaS/afridigital-backend/services/whatsapp-gateway/core/brain/index.js
+++ SaaS/afridigital-backend/services/whatsapp-gateway/core/brain/index.js
@@ -1,3 +1,5 @@
-const liveBrainV5=require('./core/brain/v5/liveBrainV5');
+const liveBrainV5=require('./v5/liveBrainV5');
 
-module.exports={processMessage:(req,res)=>liveBrainV5(req,res)};
+module.exports={
+  processMessage:(req,res)=>liveBrainV5(req,res)
+};
