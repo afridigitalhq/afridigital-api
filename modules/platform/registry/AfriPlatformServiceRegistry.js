a815c937411c5668c72eaf6ffba74eebc183a205
AfriLove Team <afriloveteam@gmail.com>
2026-07-27 06:55:12 +0100

    feat: add shared platform services runtime v1

---
+++ modules/platform/registry/AfriPlatformServiceRegistry.js
@@ -0,0 +1,15 @@
+const AfriPlatformServiceRegistry={
+  services:[
+    "AfriVerified",
+    "AfriTrust",
+    "AfriTick",
+    "AfriAI",
+    "AfriAds",
+    "AfriCoin",
+    "AfriWallet",
+    "Notifications",
+    "Analytics"
+  ]
+};
+
+export default AfriPlatformServiceRegistry;
