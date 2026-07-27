3eb204d66a4c542ed3d66387a8c415dd5a1bb95c
AfriLove Team <afriloveteam@gmail.com>
2026-07-27 07:07:21 +0100

    feat: add platform role registry runtime v1

---
+++ modules/platform/roles/AfriPlatformRoles.js
@@ -0,0 +1,14 @@
+const AfriPlatformRoles = [
+  "SUPER_ADMIN",
+  "PLATFORM_ADMIN",
+  "OFFICIAL_ACCOUNT",
+  "MODERATOR",
+  "SUPPORT",
+  "BUSINESS",
+  "SELLER",
+  "CREATOR",
+  "USER",
+  "GUEST"
+];
+
+export default AfriPlatformRoles;
