d8481e87b4ac11f09a63f45ec49b41d61458f970
AfriLove Team <afriloveteam@gmail.com>
2026-08-05 16:54:01 +0100

    feat: add AfriDebug runtime bootstrap

--- modules/afridebug/index.js
+++ modules/afridebug/index.js
@@ -1,3 +1,5 @@
-const AfriDebugModule={name:"AfriDebug",version:"1.0.0",services:{}};
+import AfriDebugRuntime from "./runtime/AfriDebugRuntime.js";
+
+const AfriDebugModule={name:"AfriDebug",version:"1.0.0",runtime:AfriDebugRuntime,services:{}};
 
 export default AfriDebugModule;
---
+++ modules/afridebug/runtime/AfriDebugRuntime.js
@@ -0,0 +1,3 @@
+const AfriDebugRuntime={boot(){return{service:"AfriDebug",status:"READY",version:"1.0.0",workspace:true,payments:true,subscriptions:true};}};
+
+export default AfriDebugRuntime;
