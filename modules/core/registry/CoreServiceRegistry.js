18dfcb0910684d85ac0c7eba41096107b4732d5d
AfriLove Team <afriloveteam@gmail.com>
2026-08-05 20:41:42 +0100

    feat: register global event services in core registry

--- modules/core/registry/CoreServiceRegistry.js
+++ modules/core/registry/CoreServiceRegistry.js
@@ -3,7 +3,8 @@ const CoreServiceRegistry={services:{
   artifacts:["CoreArtifactManager","CorePackageBuilder","CoreExportEngine","CoreDownloadManager"],
   evidence:["CoreEvidenceVault","CoreBeforeSnapshot","CoreAfterSnapshot","CoreComparisonEngine"],
   security:["CoreSecurityScanner","CoreFileScanner","CoreDependencyScanner","CoreSecretScanner","CoreIntegrityChecker","CoreSandboxValidator"],
-  identity:["CoreUserManager","CoreOrganizationManager","CoreWorkspaceManager","CoreRoleManager","CorePermissionManager","CoreSessionManager","CoreAPIKeyManager","CoreIdentityAudit"]
+  identity:["CoreUserManager","CoreOrganizationManager","CoreWorkspaceManager","CoreRoleManager","CorePermissionManager","CoreSessionManager","CoreAPIKeyManager","CoreIdentityAudit"],
+  events:["CoreEventBus","CoreEventRegistry","CoreNotificationEngine","CoreWebhookManager","CoreMessageQueue"]
 }};
 
 export default CoreServiceRegistry;
