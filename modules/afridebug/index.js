86130314de5428232cf11e968e5b03136e52f00d
AfriLove Team <afriloveteam@gmail.com>
2026-08-05 16:54:53 +0100

    feat: add AfriDebug product manifest

--- modules/afridebug/index.js
+++ modules/afridebug/index.js
@@ -1,5 +1,6 @@
 import AfriDebugRuntime from "./runtime/AfriDebugRuntime.js";
+import AfriDebugManifest from "./manifest/AfriDebugManifest.js";
 
-const AfriDebugModule={name:"AfriDebug",version:"1.0.0",runtime:AfriDebugRuntime,services:{}};
+const AfriDebugModule={name:"AfriDebug",version:"1.0.0",manifest:AfriDebugManifest,runtime:AfriDebugRuntime,services:{}};
 
 export default AfriDebugModule;
---
+++ modules/afridebug/manifest/AfriDebugManifest.js
@@ -0,0 +1,3 @@
+const AfriDebugManifest={id:"afridebug",name:"AfriDebug",version:"1.0.0",status:"ACTIVE",runtime:"AfriDebugRuntime",gateway:"/api/afridebug",workspace:true,payments:true,subscriptions:true,approvalGate:true};
+
+export default AfriDebugManifest;
