c93b4f3bfa585469f3f987f274d2637aa23b6701
AfriLove Team <afriloveteam@gmail.com>
2026-08-05 19:40:07 +0100

    feat: add AfriTest validation lifecycle foundation

---
+++ modules/afridev/products/afritest/validation/AfriTestWorkflow.js
@@ -0,0 +1,13 @@
+const AfriTestWorkflow={
+  name:"AfriTest Validation Lifecycle",
+  stages:[
+    "Receive Fix Result",
+    "Run Automated Tests",
+    "Regression Validation",
+    "Security Validation",
+    "Generate Test Report",
+    "Approve For Deploy"
+  ]
+};
+
+export default AfriTestWorkflow;
